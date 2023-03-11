3ed40ee2408cbefeeac8c426a137db7999c5d11f
Dayvid Santana da Silva <77745454+Dayvid-San@users.noreply.github.com>
2023-03-11 01:13:40 -0300

    feat: added for to list in else

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-def cifra():
+def leitor(lista, aux = 0):
+    if lista == []:
+        return lista
+    else:
+        aux += 1
+        leitor(lista[aux], aux)
+
+
+def cifra(lista = []):
     num = int(input('Qual o número?\n'))
-    res = []
-    res.append(num)
+    lista.append(num)
 
     p = str(input('Vai adicionar outro número?\n'))
     if p == 'y':
         cifra()
     else:
-        print('Este são os números: {}'.format(res))
+        aux = 0
+        listFound = []
 
+        for i in lista:
+            restor = i * '{}'.format(aux)
+            aux += 1
+            return listFound.append(restor)
 
+        print('Este são os números: {}'.format(listFound))
 
 
 cifra()
